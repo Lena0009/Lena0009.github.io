c1751c889e4691f03714e24b6565797126ead0f4
Lena0009 <144767912+Lena0009@users.noreply.github.com>
2024-05-14 08:44:55 +0200

    V12

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-My project (1)-1.0";
 const contentToCache = [
-    "Build/Prototype version 11.loader.js",
-    "Build/Prototype version 11.framework.js",
-    "Build/Prototype version 11.data",
-    "Build/Prototype version 11.wasm",
+    "Build/Prototype version 12.loader.js",
+    "Build/Prototype version 12.framework.js",
+    "Build/Prototype version 12.data",
+    "Build/Prototype version 12.wasm",
     "TemplateData/style.css"
 
 ];
